fixed computation of balanced accuracy

autoPyTorch/components/metrics/standard_metrics.py
@@ -1,22 +1,23 @@
 import sklearn.metrics as metrics
+import numpy as np
 
 # classification metrics
 def accuracy(y_pred, y_true):
-    return (undo_ohe(y_true) == undo_ohe(y_pred)).float().mean().item() * 100
+    return np.mean((undo_ohe(y_true) == undo_ohe(y_pred))) * 100
 
 def auc_metric(y_pred, y_true):
-    return (2 * metrics.roc_auc_score(y_true.cpu().numpy(), y_pred.cpu().numpy()) - 1) * 100
+    return (2 * metrics.roc_auc_score(y_true, y_pred) - 1) * 100
 
 
 # multilabel metric
 def multilabel_accuracy(y_pred, y_true):
-    return (y_true.long() == (y_pred > 0.5).long()).float().mean().item() * 100
+    return np.mean(y_true == (y_pred > 0.5)) * 100
 
 # regression metric
 def mean_distance(y_pred, y_true):
-    return (y_true - y_pred).abs().mean().item()
+    return np.mean(np.abs(y_true - y_pred))
 
 def undo_ohe(y):
     if len(y.shape) == 1:
         return(y)
-    return y.max(1)[1]
+    return np.argmax(y, axis=1)

autoPyTorch/pipeline/nodes/train_node.py
@@ -108,7 +108,7 @@ class TrainNode(PipelineNode):
     def predict(self, pipeline_config, network, predict_loader):
         device = torch.device('cuda:0' if pipeline_config['cuda'] else 'cpu')
         
-        Y = predict(network, predict_loader, None, device)
+        Y = predict(network, predict_loader, device)
         return {'Y': Y.detach().cpu().numpy()}
     
     def add_training_technique(self, name, training_technique):
@@ -186,7 +186,7 @@ class TrainNode(PipelineNode):
         return loss, final_log
 
 
-def predict(network, test_loader, metrics, device, move_network=True):
+def predict(network, test_loader, device, move_network=True):
     """ predict batchwise """
     # Build DataLoader
     if move_network:
@@ -194,32 +194,14 @@ def predict(network, test_loader, metrics, device, move_network=True):
 
     # Batch prediction
     network.eval()
-    if metrics is not None:
-        metric_results = [0] * len(metrics)
+    Y_batch_preds = list()
     
-    N = 0.0
     for i, (X_batch, Y_batch) in enumerate(test_loader):
         # Predict on batch
         X_batch = Variable(X_batch).to(device)
         batch_size = X_batch.size(0)
 
         Y_batch_pred = network(X_batch).detach().cpu()
-
-        if metrics is None:
-            # Infer prediction shape
-            if i == 0:
-                Y_pred = Y_batch_pred
-            else:
-                # Add to prediction tensor
-                Y_pred = torch.cat((Y_pred, Y_batch_pred), 0)
-        else:
-            for i, metric in enumerate(metrics):
-                metric_results[i] += metric(Y_batch, Y_batch_pred) * batch_size
-
-        N += batch_size
+        Y_batch_preds.append(Y_batch_pred)
     
-    if metrics is None:
-        return Y_pred
-    else:
-        return [res / N for res in metric_results]
-
+    return torch.cat(Y_batch_preds, 0)

autoPyTorch/training/trainer.py
@@ -1,6 +1,7 @@
 import time
 import os
 import torch
+import numpy as np
 
 from torch.autograd import Variable
 from autoPyTorch.utils.configspace_wrapper import ConfigWrapper
@@ -103,8 +104,9 @@ class Trainer(object):
         loss_sum = 0.0
         N = 0
         self.model.train()
+        outputs_data = list()
+        targets_data = list()
 
-        metric_results = [0] * len(self.metrics)
         for step, (data, targets) in enumerate(train_loader):
    
             # prepare
@@ -126,46 +128,40 @@ class Trainer(object):
             loss.backward()
             self.optimizer.step()
 
-            # evaluate metrics
-            for i, metric in enumerate(self.metrics):
-                metric_results[i] += self.loss_computation.evaluate(metric, outputs, **criterion_kwargs) * batch_size
+            # save for metric evaluation
+            outputs_data.append(outputs.data.cpu().detach().numpy())
+            targets_data.append(targets.data.cpu().detach().numpy())
 
             loss_sum += loss.item() * batch_size
             N += batch_size
 
             if any([t.on_batch_end(batch_loss=loss.item(), trainer=self, epoch=epoch, step=step, num_steps=len(train_loader))
                     for t in self.training_techniques]):
-                return [res / N for res in metric_results], loss_sum / N, True
-
-        return [res / N for res in metric_results], loss_sum / N, False
+                return self.compute_metrics(outputs_data, targets_data), loss_sum / N, True
+        return self.compute_metrics(outputs_data, targets_data), loss_sum / N, False
 
 
     def evaluate(self, test_loader):
-
-        N = 0
-        metric_results = [0] * len(self.metrics)
-        
         self.model.eval()
+
+        outputs_data = list()
+        targets_data = list()
 
         with torch.no_grad():
             for _, (data, targets) in enumerate(test_loader):
     
                 data = data.to(self.device)
-                targets = targets.to(self.device)
-
                 data = Variable(data)
-                targets = Variable(targets)
-                
-                batch_size = data.size(0)
-
                 outputs = self.model(data)
-                
-                for i, metric in enumerate(self.metrics):
-                    metric_results[i] += metric(outputs.data, targets.data) * batch_size
 
-                N += batch_size
+                outputs_data.append(outputs.data.cpu().detach().numpy())
+                targets_data.append(targets.data.cpu().detach().numpy())
 
         self.model.train()
-            
-        return [res / N for res in metric_results]
+        return self.compute_metrics(outputs_data, targets_data)
+    
+    def compute_metrics(self, outputs_data, targets_data):
+        outputs_data = np.vstack(outputs_data)
+        targets_data = np.vstack(targets_data)
+        return [metric(outputs_data, targets_data) for metric in self.metrics]
     