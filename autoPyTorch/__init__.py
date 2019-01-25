39bc548b36232e7e07caf8af1a7d7b1105d25cb8
Matthias Urban <urbanm@informatik.uni-freiburg.de>
2019-01-25 16:09:14 +0100

    fixed path to hpbandster submodule

--- autoPyTorch/__init__.py
+++ autoPyTorch/__init__.py
@@ -1,5 +1,6 @@
-import sys
-sys.path.append('./submodules/HpBandSter')
+import sys, os
+hpbandster = os.path.abspath(os.path.join(__file__, '..', '..', 'submodules', 'HpBandSter'))
+sys.path.append(hpbandster)
 
 from autoPyTorch.core.autonet_classes import AutoNetClassification, AutoNetMultilabel, AutoNetRegression
 from autoPyTorch.utils.hyperparameter_search_space_update import HyperparameterSearchSpaceUpdates
