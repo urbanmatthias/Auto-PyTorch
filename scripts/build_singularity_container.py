8d6914aa65724aec648f3394881cedb6d4ed01ad
Matthias Urban <urbanm@informatik.uni-freiburg.de>
2019-03-17 09:48:29 +0100

    updated build singularity script

--- scripts/build_singularity_container.py
+++ scripts/build_singularity_container.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     if move_into_container:
         subprocess.call(["tar", "-czvf", "move_into_container.tar.gz"] + move_into_container)
     image_name = input("Name of Image? (Default: Auto-PyTorch.simg) ") or "Auto-PyTorch.simg"
+    if os.path.exists(image_name) and input("%s exists. Remove (y/n)? " % image_name).startswith("y"):
+        os.remove(image_name)
     print("Building Singularity container. You need to be root for that.")
     subprocess.call(["sudo", "singularity", "build", image_name, "scripts/Singularity"])
     if move_into_container:
