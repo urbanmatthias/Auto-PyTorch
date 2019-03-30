cb4113341bf58b101224e9a06ed4e6cece3dcd8c
Matthias Urban <urbanm@informatik.uni-freiburg.de>
2019-03-30 15:52:37 +0100

    config up hpbandster up

--- scripts/plot_weight_history.py
+++ scripts/plot_weight_history.py
@@ -7,37 +7,31 @@ sum_of_weights_history = None
 if __name__ == "__main__":
     weight_history_file = sys.argv[-1]
     with open(weight_history_file, "r") as f:
-        data = next(f).split(": ")[1]
-        data = list(map(float, map(str.strip, data.split(","))))
-        plt.title("Sum of weights history")
-        plt.plot(list(range(len(data))), data)
-        plt.show()
-        next(f)
-
         show_labels=dict()
         for line in f:
-            line = line.split(": ")
+            line = line.split("\t")
+            if len(line) == 1 or not line[-1].strip():
+                continue
             data = line[-1]
             data = list(map(float, map(str.strip, data.split(","))))
-            title = ": ".join(line[:-1])
+            title = "\t".join(line[:-1])
             plt.plot(list(range(len(data))), data, label=title)
             show_labels[title] = np.sum(data)
-        plt.ylim((-0.05, 1.05))
         plt.show()
-        show_labels = sorted(show_labels.keys(), key=lambda x: -show_labels[x])[:10]
+        plt.title("Weight history")
+        show_labels = sorted(show_labels.keys(), key=lambda x: -show_labels[x])[:5]
 
     with open(weight_history_file, "r") as f:
-        next(f)
-        next(f)
-
         for line in f:
-            line = line.split(": ")
+            line = line.split("\t")
+            if len(line) == 1 or not line[-1].strip():
+                continue
             data = line[-1]
             data = list(map(float, map(str.strip, data.split(","))))
-            title = ": ".join(line[:-1])
+            title = "\t".join(line[:-1])
             if title in show_labels:
                 plt.plot(list(range(len(data))), data, label=title)
 
         plt.legend(loc='best')
-        plt.ylim((-0.05, 1.05))
+        plt.title("Weight history")
         plt.show()
