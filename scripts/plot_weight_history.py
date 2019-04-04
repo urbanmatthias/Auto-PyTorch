9743a4057e0babd906c07153ab7f76ddcbd3c498
Matthias Urban <urbanm@informatik.uni-freiburg.de>
2019-04-04 14:21:46 +0200

    made plot weight history more efficient

--- scripts/plot_weight_history.py
+++ scripts/plot_weight_history.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import argparse
 import numpy as np
 import os, sys
+import heapq
 
 hpbandster = os.path.abspath(os.path.join(__file__, '..', '..', 'submodules', 'HpBandSter'))
 sys.path.append(hpbandster)
@@ -22,24 +23,32 @@ if __name__ == "__main__":
     
     weight_deviation_history = list()
     weight_deviation_timestamps = list()
+    
+    #iterate over all files and lot the weight history
     for i, weight_history_file in enumerate(args.weight_history_files):
         print(i)
         plot_data = dict()
         with open(weight_history_file, "r") as f:
             for line in f:
+                
+                #read the data
                 line = line.split("\t")
                 if len(line) == 1 or not line[-1].strip():
                     continue
                 data = line[-1]
                 data = list(map(float, map(str.strip, data.split(","))))
                 title = "\t".join(line[:-1]).strip()
+                
+                # and save it later for plotting
                 plot_data[title] = data
+                
+        # only show labels for top datasets
         sorted_keys = sorted(plot_data.keys(), key=lambda x, d=plot_data: -d[x][-1] if x != "current" else -float("inf"))
         show_labels = set(sorted_keys[:args.max_legend_size])
         consider_in_summary = set(sorted_keys[:args.num_consider_in_summary])
 
+        # parse results to get the timestamps for the weights
         x_axis = []
-        x_scale = "linear"
         try:
             r = logged_results_to_HBS_result(os.path.dirname(weight_history_file))
             sampled_configs = set()
@@ -47,10 +56,10 @@ if __name__ == "__main__":
                 if run.config_id not in sampled_configs:
                     x_axis.append(run.time_stamps["submitted"])
                 sampled_configs |= set([run.config_id])
-            x_scale = "log"
         except Exception as e:
-            print(e)
+            continue
 
+        # do the plotting
         if not args.only_summary:
             for title, data in sorted(plot_data.items()):
                     plt.plot(x_axis or list(range(len(data))), data,
@@ -60,40 +69,36 @@ if __name__ == "__main__":
 
             plt.legend(loc='best')
             plt.title(weight_history_file)
-            plt.xscale(x_scale)
+            plt.xscale("log")
             plt.show()
-        
+
+        # save data for summary
         for title, data in plot_data.items():
             if title in consider_in_summary:
                 weight_deviation_history.append([abs(d - data[-1]) for d in data])
+                weight_deviation_timestamps.append(x_axis)
 
-                if not x_axis:
-                    weight_deviation_timestamps = None
-
-                if weight_deviation_timestamps is not None:
-                    weight_deviation_timestamps.append(x_axis)
-    
+    # plot summary
     weight_deviation_history = np.array(weight_deviation_history)
-
-    if weight_deviation_timestamps is None:
-        plt.plot(range(weight_deviation_history.shape[1]), np.mean(weight_deviation_history, axis=0))
-        plt.title("weight deviation over time")
-        plt.show()
-        exit(0)
-    
     weight_deviation_timestamps = np.array(weight_deviation_timestamps)
+    
+    # iterate over all weight deviation histories simultaneously, ordered by increasing timestamps
     history_pointers = [0] * weight_deviation_timestamps.shape[0]
     current_values = [None] * weight_deviation_timestamps.shape[0]
+    heap = [(weight_deviation_timestamps[i, 0], weight_deviation_history[i, 0], i, 0)  # use heap to sort by timestamps
+            for i in range(weight_deviation_timestamps.shape[0])]
+    heapq.heapify(heap)
 
     times = []
     values = []
-    while any(p < weight_deviation_timestamps.shape[1] for p in history_pointers):
-        time, v, i, p = min((weight_deviation_timestamps[i, p], weight_deviation_history[i,p], i, p)
-            for i, p in enumerate(history_pointers) if p < weight_deviation_timestamps.shape[1])
+    while heap:
+        time, v, i, p = heapq.heappop(heap)
         current_values[i] = v
         values.append(np.mean([v for v in current_values if v is not None]))
         times.append(time)
         history_pointers[i] += 1
+        if p + 1 < weight_deviation_timestamps.shape[1]:
+            heapq.heappush(heap, (weight_deviation_timestamps[i, p + 1], weight_deviation_history[i, p + 1], i, p + 1))
 
     plt.plot(times, values, marker="x")
     plt.title("weight deviation over time")
