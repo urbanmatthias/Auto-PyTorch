beb3e877566686101fcdbe5048938632073ed076
Matthias_Urban <urbanm@tf.uni-freiburg.de>
2019-05-27 16:06:37 +0200

    Added moab script to compute plots

--- scripts/plot_weight_history.py
+++ scripts/plot_weight_history.py
@@ -1,4 +1,3 @@
-from hpbandster.core.result import logged_results_to_HBS_result
 import argparse
 import heapq
 import os
@@ -8,8 +7,8 @@ import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
 
-sys.path.append(os.path.abspath(os.path.join(
-    __file__, '..', '..', 'submodules/HpBandSter')))
+sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..', 'submodules/HpBandSter')))
+
 
 
 sum_of_weights_history = None
@@ -17,14 +16,13 @@ sum_of_weights_history = None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run benchmarks for autonet.')
-    parser.add_argument("weight_history_files", type=str,
-                        nargs="+", help="The files to plot")
+    parser.add_argument("weight_history_files", type=str, nargs="+", help="The files to plot")
 
     args = parser.parse_args()
 
     weight_deviation_history = list()
     weight_deviation_timestamps = list()
-
+    
     all_plot_data = list()
     all_additional_info = list()
 
@@ -55,10 +53,9 @@ if __name__ == "__main__":
                     additional_info_names |= set([title])
 
                 current_read = current_read or title == "current"
-
+                
         # only show labels for top datasets
-        sorted_keys = sorted(plot_data.keys(
-        ), key=lambda x, d=plot_data: -d[x][-1] if x != "current" else -float("inf"))
+        sorted_keys = sorted(plot_data.keys(), key=lambda x, d=plot_data: -d[x][-1] if x != "current" else -float("inf"))
 
         # parse results to get the timestamps for the weights
         x_axis = []
@@ -88,24 +85,22 @@ if __name__ == "__main__":
     # do the plotting
     num_subplots = (1 + len(additional_info_names)) * 100
     gridshape = (max(1, len(additional_info_names) * 4), 1)
-    ax1 = plt.subplot2grid(
-        gridshape, (0, 0), rowspan=max(1, len(additional_info) * 3))
+    ax1 = plt.subplot2grid(gridshape, (0, 0), rowspan=max(1, len(additional_info) * 3))
 
     for plot_data in all_plot_data:
         for title, (x_axis, data) in plot_data.items():
             plt.plot(x_axis, data[:len(x_axis)], lw=0.1 if title != "current" else 0.5,
-                     color="red" if title == "current" else "blue")
+                color="red" if title == "current" else "blue")
 
     plt.title("weight history")
     plt.xscale("log")
 
     for i, title in enumerate(sorted(additional_info_names)):
-        ax = plt.subplot2grid(
-            gridshape, (int(gridshape[0] * 3/4) + i, 0), sharex=ax1)
+        ax = plt.subplot2grid(gridshape, (int(gridshape[0] * 3/4) + i, 0), sharex=ax1)
         for j, additional_info in enumerate(all_additional_info):
             x_axis, data = additional_info[title]
-            plt.scatter(x_axis, data[:len(
-                x_axis)], label=title if j == 0 else None, marker="x", color="orange")
+            plt.scatter(x_axis, data[:len(x_axis)], label=title if j == 0 else None, marker="x", color="orange")
         plt.legend(loc='upper left')
+
 
     plt.show()
